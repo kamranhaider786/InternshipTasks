5fa8e3920c1ddd0e55cd272205beb2f7653bb29d
kamranhaider786 <www.kamranhaider4539@gmail.com>
2023-08-21 11:29:58 +0500

    new folder tutorial 12

---
+++ Tutorial12/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+
+// npx tailwindcss init configFile --full
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      xsm: '500px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+
+      spacing: {
+        13: '3rem'
+      },
+      fontSize: {
+        '10xl': ['9rem', { lineHeight: '1.2' }]
+      }
+
+    },
+  },
+  plugins: [],
+}
+
